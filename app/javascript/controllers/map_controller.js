5521f71ea012dad124ecd2655a33a1c2b90c97c8
IkuroMaira <gwen.bussac@gmail.com>
2025-05-26 11:44:09 +0200

    Add CRUD spots

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -3,7 +3,7 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
     static targets = ["map"]
     connect() {
-        console.log("Map controller connecté !")
+        console.log("Map controller connecté !");
         this.initializeMap()
     }
 
