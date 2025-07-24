105d7296e75c75d0cf4e51de0497135572fcb236
Gwenaëlle <gwenaelle.bussac@simundia.com>
2025-07-24 15:41:37 +0200

    Add Ics Generator Service, generate EventMailer

--- app/services/ics_generator.rb
+++ app/services/ics_generator.rb
@@ -6,16 +6,14 @@ class IcsGenerator
   def generate
     calendar = Icalendar::Calendar.new
     calendar.event do |e|
-
       # Propriétés obligatoires
       e.dtstart = @event.start_time
       e.dtend = @event.end_time
       e.summary = @event.title
       # Aujouter d'autres propriétés
-
     end
-  end
 
-  # La méthode .to_ical génère le texte au format ICS standard
-  calendar.to_ical
+    # La méthode .to_ical génère le texte au format ICS standard
+    calendar.to_ical
+  end
 end
