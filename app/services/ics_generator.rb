c423078dfe3eba42d64877079a7165d220dd990d
Gwenaëlle <gwenaelle.bussac@simundia.com>
2025-07-24 14:43:05 +0200

    Add simple and basic version of IcsGenerator

---
+++ app/services/ics_generator.rb
@@ -0,0 +1,21 @@
+class IcsGenerator
+  def initialize(event)
+    @event = event
+  end
+
+  def generate
+    calendar = Icalendar::Calendar.new
+    calendar.event do |e|
+
+      # Propriétés obligatoires
+      e.dtstart = @event.start_time
+      e.dtend = @event.end_time
+      e.summary = @event.title
+      # Aujouter d'autres propriétés
+
+    end
+  end
+
+  # La méthode .to_ical génère le texte au format ICS standard
+  calendar.to_ical
+end
