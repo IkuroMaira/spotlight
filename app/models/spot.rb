9a70c8b12a7bd4781b9123c156c16a1d04331d00
Gwenaëlle <gwenaelle.bussac@simundia.com>
2026-01-19 11:44:20 +0100

    WIP

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -10,8 +10,23 @@ class Spot < ApplicationRecord
   has_and_belongs_to_many :tags
   has_and_belongs_to_many :collections
 
-  validates :name, presence: true
-  # validates :latitude, presence: true #
-  # validates :longitude, presence: true
-  # validates :is_social_impact, presence: false # Je m'interroge dessus
+  validates :name, presence: { message: "Ne peut pas être vide" },
+                   length: { in: 2..100 },
+                   uniqueness: { scope: :user_id, message: "Vous avez déjà un spot avec ce nom" }
+  validates :description, length: { maximum: 1000 }
+  validates :description, presence: true, if: :social_impact?
+  validates :address, presence: true, length: { maximum: 255 }
+  validates :latitude, presence: true, numericality: {
+    greater_than_or_equal_to: -90,
+    less_than_or_equal_to: 90
+  }
+  validates :longitude, presence: true, numericality: {
+    greater_than_or_equal_to: -180,
+    less_than_or_equal_to: 180
+  }
+
+  private
+  def social_impact?
+    is_social_impact
+  end
 end
